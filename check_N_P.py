a25b4767f8e05ebab1f616f26f2d09b5498e348f
Saikumar <182195561+saikumar0118@users.noreply.github.com>
2026-08-16 00:54:04 +0530

    Delete check_N_P.py

--- check_N_P.py
+++
@@ -1,7 +0,0 @@
-x=int(input("Enter a number"))
-if x>0:
-    print("Enter the number is positive")
-elif x==0:
-    print("Enter the number is Zero")
-else:
-    print("Enter the number is negative ")
